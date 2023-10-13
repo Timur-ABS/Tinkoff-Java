77d59b15bea4f3eb4db6313bc094531929451cf0
Timur <Timurkaabs@gmail.com>
2023-10-13 22:22:15 +0300

    hw1 correction9

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -1,25 +1,14 @@
-package edu.hw1;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public final class Task0 {
-    private final static Logger LOGGER = LogManager.getLogger();
-
-    private Task0() {
-        // Private constructor to prevent instantiation
-    }
-
-    //
-    //Entry point for the Task0 program.
-    //Logs a welcome message to the console.
-    //
-    public static void main(String[] args) {
-        //
-        //Entry point for the Task0 program.
-        //Logs a welcome message to the console.
-        //
-        LOGGER.info("Hello and welcome!");
-    }
-
-}
+//package edu.hw1;
+//
+//public final class Task0 {
+////    private final static Logger LOGGER = LogManager.getLogger();
+//
+//    private Task0() {
+//        // Private constructor to prevent instantiation
+//    }
+//
+////    public static void main(String[] args) {
+////        LOGGER.info("Hello and welcome!"); //maven checkstyle не проходит
+////    }
+//
+//}
--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -1,27 +1,11 @@
 package edu.hw1;
 
-import java.util.Scanner;
-import java.util.logging.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public final class Task1 {
     private static final int SECONDS_IN_A_MINUTE = 60;
-    private static final Logger LOGGER = Logger.getLogger(Task1.class.getName());
 
     private Task1() {
-    }
-
-    //Entry point for the Task1 program.
-    //This method prompts the user to input a time string in the format "minutes:seconds".
-    //It then converts this string to total seconds and logs the result using the LOGGER.
-    public static void main(String[] args) {
-        //Entry point for the Task1 program.
-        //This method prompts the user to input a time string in the format "minutes:seconds".
-        //It then converts this string to total seconds and logs the result using the LOGGER.
-        Scanner in = new Scanner(System.in);
-        String string = in.nextLine();
-        int result = convertMinutesToSeconds(string);
-        LOGGER.info(String.valueOf(result));
     }
 
     public static int convertMinutesToSeconds(@NotNull String input) {
