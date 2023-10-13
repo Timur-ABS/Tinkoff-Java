ed29ce5edde1597081d0c306f0e8e9836ec4159c
Timur <Timurkaabs@gmail.com>
2023-10-13 22:30:21 +0300

    hw1 correction10

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -1,14 +1,21 @@
-//package edu.hw1;
-//
-//public final class Task0 {
-////    private final static Logger LOGGER = LogManager.getLogger();
-//
-//    private Task0() {
-//        // Private constructor to prevent instantiation
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class Task0 {
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    private Task0() {
+        // Private constructor to prevent instantiation
+    }
+
+    /**
+     * Entry point for the Task0 program.
+     * Logs a welcome message to the console.
+     */
+//    public static void main(String[] args) {
+//        LOGGER.info("Hello and welcome!");
 //    }
-//
-////    public static void main(String[] args) {
-////        LOGGER.info("Hello and welcome!"); //maven checkstyle не проходит
-////    }
-//
-//}
+
+}
